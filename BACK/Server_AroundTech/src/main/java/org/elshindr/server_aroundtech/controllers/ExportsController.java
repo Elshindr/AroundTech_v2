ce1514200adb40821e076b5011ca87ade798072c
Elshindr <headkeytics@gmail.com>
2024-03-07 22:22:01 +0100

    Update ExportsController.java

--- BACK/Server_AroundTech/src/main/java/org/elshindr/server_aroundtech/controllers/ExportsController.java
+++ BACK/Server_AroundTech/src/main/java/org/elshindr/server_aroundtech/controllers/ExportsController.java
@@ -25,27 +25,5 @@ public class ExportsController {
 
 
 
-    @GetMapping("/telechargerFichierExcel")
-    public ResponseEntity<byte[]> telechargerFichierExcel() {
-        try {
-            // Générer votre fichier Excel
-            Workbook workbook = generateExcelFile();
-
-            // Convertir le classeur en un tableau d'octets
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            workbook.write(outputStream);
-
-            // Ajouter les en-têtes pour indiquer au navigateur que le contenu est un fichier Excel téléchargeable
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData("attachment", "mesprimes.xlsx");
-
-            // Retourner une réponse avec le contenu du fichier Excel
-            return ResponseEntity.ok().headers(headers).body(outputStream.toByteArray());
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Gérer l'erreur ici
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
+    
 }
